a99472e3cc0b308ebffc041c0d34681e5c7f4d88
clickad <adnan.damadzic@gmail.com>
2019-08-01 09:38:34 +0200

    Babel compiled

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,18 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
-      }
+      },
+	  {
+		test: /\.js$/,
+		exclude: /(node_modules|bower_components)/,
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: ["@babel/preset-env"]
+			}
+		}
+		}
+	  
     ]
   }
 };
